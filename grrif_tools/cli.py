93dffc91522fe98ceac946c51db598690d100bae
Julien <6372605+fetzu@users.noreply.github.com>
2023-05-07 23:11:39 +0200

    fix(build): fixed issue with entry-points in pyproject.toml
    * bumped to 0.6.1

--- grrif_tools/cli.py
+++ grrif_tools/cli.py
@@ -5,7 +5,7 @@ import argparse
 from datetime import date, datetime
 
 ## [ CONFIGURATION ]
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 ## [ Is CLI even cooler with argparse? ]
 parser = argparse.ArgumentParser(
