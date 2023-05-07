eccc4fb9b39c20d258936a867963dc091ab10042
Julien <6372605+fetzu@users.noreply.github.com>
2023-05-07 21:04:45 +0200

    chore: bumped to 0.5.2
    * Nothing changed, just testing...

--- grrif_tools/cli.py
+++ grrif_tools/cli.py
@@ -5,7 +5,7 @@ import argparse
 from datetime import date, datetime
 
 ## [ CONFIGURATION ]
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 ## [ Is CLI even cooler with argparse? ]
 parser = argparse.ArgumentParser(
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description="A set of tools for Cool Cats™. Allows you to archive GRRIF's play history to a SQLite database or text files, and maybe more...",
     author="Julien 'fetzu' Bono", 
     url="https://fetzu.ch/",
-    version="0.5.1",
+    version="0.5.2",
     download_url="https://github.com/fetzu/grrif_tools",
     packages=find_packages(include=['grrif_tools','grrif_tools.*']),
     license="License :: OSI Approved :: MIT License",
