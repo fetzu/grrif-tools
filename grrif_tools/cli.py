632fc06a1c44e7694567603b08f73adc83baa6e9
Julien <6372605+fetzu@users.noreply.github.com>
2023-07-23 18:21:19 +0200

    chore: bump to 0.8.1

--- grrif_tools/cli.py
+++ grrif_tools/cli.py
@@ -5,7 +5,7 @@ import argparse
 from datetime import date, datetime
 
 ## [ CONFIGURATION ]
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 ## [ Is CLI even cooler with argparse? ]
 parser = argparse.ArgumentParser(
